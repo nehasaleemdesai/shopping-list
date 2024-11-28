478d98b62c28606b2dbc9ba074aab37101763905
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2024-11-28 00:13:32 -0600

    included local storage for add items

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const itemList = document.getElementById("item-list");
 const clearButton = document.getElementById("clear");
 const itemFilter = document.getElementById("filter");
 
-const addItem = (e) => {
+// add item on submit
+const onAddItemSubmit = (e) => {
   e.preventDefault();
 
   const newItem = itemInput.value;
@@ -13,18 +14,42 @@ const addItem = (e) => {
     alert("Please add an item");
     return;
   }
+
+  // create item DOM element
+  addItemToDOM(newItem);
+
+  // add item to local storage
+  addItemToStorage(newItem);
+  checkUI();
+  itemInput.value = "";
+};
+
+function addItemToDOM(item) {
   // create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
   // add li to the DOM
   itemList.appendChild(li);
-  checkUI();
-  itemInput.value = "";
-};
+}
+
+function addItemToStorage(item) {
+  let itemsFromStorage;
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
+  // add new item to array
+  itemsFromStorage.push(item);
+
+  // conver to json string and set to local storage
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
 const createButton = (classes) => {
   const button = document.createElement("button");
   button.className = classes;
@@ -70,9 +95,23 @@ function checkUI() {
   }
 }
 
+function filterItems(e) {
+  const text = e.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) !== -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
+itemForm.addEventListener("submit", onAddItemSubmit);
 itemList.addEventListener("click", removeItem);
 clearButton.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
 
 checkUI();
